ade939fa012542255545796d3a654fbbc4eef419
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-04 15:35:53 +0530

    variable

---
+++ 03-variable.sh
@@ -0,0 +1,12 @@
+#variables 
+
+## pass values when executing scripts
+
+person=$1
+person2=$2
+course=$3
+
+ echo "$1 : hi $2"
+ echo "$2: hello $1, what are learning"
+ echo "$1 : i am learning $3, what about you"
+ echo "$2 :  i am also learning $3" 
