75a564ac521f4313bfb23cbdf75024676b806e8a
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-04 15:22:37 +0530

    variable

---
+++ 02-variable.sh
@@ -0,0 +1,8 @@
+## Variables
+
+#Arguments without varibables
+ 
+ echo "suresh : hi dinesh"
+ echo "dinesh : hello suresh, what are learning"
+ echo "suresh : i am learning shell, what about you"
+ echo "dinesh :  i am also learning shell" 
