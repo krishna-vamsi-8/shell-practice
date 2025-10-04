211a88b223fdd1a1afd970b38858fb105663f9b6
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-04 15:59:56 +0530

    Read

--- 04-read.sh
+++ 04-read.sh
@@ -3,6 +3,13 @@
 # Read Command
 
 echo "enter your pin number"
-read pin_number
-
+read pin_number #pin_number is variable
 echo "you enter pin number is: $pin_number"
+
+
+
+# Read Command with option to hide entered pin number
+
+echo "enter your pin number"
+read -s Pin_Number #pin_number is variable
+echo "you enter pin number is: $Pin_Number"
