864084caddfebddc72dd587965f05fccc784979a
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-05 16:12:18 +0530

    data type

--- 07-datatype.sh
+++ 07-datatype.sh
@@ -3,7 +3,16 @@
 ## Data Types
 ## no data type is shell scripting
 ## shell consider all values as string
+## Can use $variable_names or ${variable_name} to declare variable
+
+## Array
+# count based index values
+# index values start from 0 
 
 friends=("anith" "dinesh" "avinash" "nara" "sharu")
 
-echo "tell your friends name: ${friends}"  ## Can use $variable_names or ${variable_name}
+echo "tell your friends name: ${friends}"  ## It prints starting index
+
+echo "tell your friends name: ${friends [@]}"  ## It prints all values 
+
+echo "tell your friends name: ${friends [2]}"  ## It prints based on given index value
