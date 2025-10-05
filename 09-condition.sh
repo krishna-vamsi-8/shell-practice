f5c80aeb0b756a7ab441ebf1608d41a0b806505f
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-05 19:36:12 +0530

    conditions

--- 09-condition.sh
+++ 09-condition.sh
@@ -2,18 +2,19 @@
 
 ## Condition
 
+# -lt less than
+# -ne not equal
+# -eq equal
+# -gt greater than
+
 echo " To vote you age should be above 18"
 echo "tell us your age"
 read age
-echo "show your proof of age"
-read id ## if you have id enter yes or else enter no
-
-
 
 if [ $age -gt 18 ]; then
   echo "you can vote"
-elif [ $id -eq yes ]; then
-  echo "you are eligible to vote"
+elif [ $age -lt 18]; then
+  echo "you are not eligible to vote"
 else
   echo "you are welcome"
 fi
