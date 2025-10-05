da02457f59c7ae9a3a6e391b14c76eb116141a2d
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-05 16:06:56 +0530

    data type

---
+++ 07-datatype.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Data Types
+## no data type is shell scripting
+## shell consider all values as string
+
+friends=("anith" "dinesh" "avinash" "nara" "sharu")
+
+echo "tell your friends name: ${friends}"  ## Can use $variable_names or ${variable_name}
