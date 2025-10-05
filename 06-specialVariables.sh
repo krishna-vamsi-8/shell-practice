c1513989d31549b793895dd73d939745704fe530
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-05 15:44:12 +0530

    Special Variables

--- 06-specialVariables.sh
+++ 06-specialVariables.sh
@@ -2,7 +2,22 @@
 
 ## Special Variables
 
-echo "enter the required Names: $@"  ##  $@  is used to enter all
+echo "enter the required Names: $@"  ##  $@  is used to enter data at time of execution of script
 
 
-echo "enter the required Names: $*"  ##  $*  is used to enter all 
+echo "enter the required Names: $*"  ##  $*  is used to enter data at time of execution of script
+
+echo "show last executed script name: $0"
+
+echo "current directory : $pwd "
+
+echo "Who Am I: $user"
+
+echo "Home Directory: $home"
+
+echo "Process ID of executed Script: $$" ## show PID of current executed Script 
+
+sleep 10
+
+echo "PID of last executed script: $!" ## show PID of last executed Script if it is in background 
+
