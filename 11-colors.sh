48abe1db7584fbe1c5efb91bc24633630181a9af
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-06 14:12:18 +0530

    color

--- 11-colors.sh
+++ 11-colors.sh
@@ -2,14 +2,15 @@
 
  ##color are declared in variables
 R="\e[31m"
-G="\e32[m"
-Y="\e33[m"
+G="\e[32m"
+Y="\e[33m"
+
 
 ## This is a color customization 
  
  ## Use -e to enable the color option
  ## Use $N to not continue same color for next line ## In simple term to break
- 
+
 echo -e "Hi this is a superhero $R Red $N"
 echo -e "Hi this is a superhero $G green $N"
 echo -e "Hi this is a superhero $Y yellow $N"
