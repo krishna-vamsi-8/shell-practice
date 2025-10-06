8c5168c6c657b3b3067a3167cfc65f2f4f682097
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-06 15:39:07 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -9,7 +9,6 @@ if [ $user -ne 0 ]; then
   echo "you are not root user"
   exit 1
 fi
-
 validate(){
 if [ $1 -ne 0 ]; then
   echo -e " $2  $r Not installed $n"
@@ -17,11 +16,11 @@ if [ $1 -ne 0 ]; then
 else
   echo -e "$2 is  $y successfully installed $n"
 fi  }
-
 dnf list installed $2
 if [ $1 -ne 0 ]; then
  dnf install $@
  validate $? $@
+ exit 1
 else
 echo -e " $g already installed $n" 
 fi
