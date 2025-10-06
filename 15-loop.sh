ef5e409172088a94df1b6e6ca10e2bf63fa8e903
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-06 16:37:55 +0530

    loop

--- 15-loop.sh
+++ 15-loop.sh
@@ -3,16 +3,14 @@
 y="\e[33m"
 r="\e[31m"
 n="\e[0m"
-
-logfolder=var/log/shellscipt  ## path to create directory
+user=$(id -u)
+logfolder="/var/log/shellscipt"  ## path to create directory
 scriptname=$( echo $0 | cut -d "." -f1 ) ## $0 tells last executed script name
 logfile=$logfolder/$scriptname.log
 
-
 mkdir -p $logfolder
- echo "script start time $(date +%d-%m-%y)" | tee -a $logfile
+echo "script start time $(date +%d-%m-%y)" | tee -a $logfile
 ## checking the user 
-user=$(id -u)
 if [ $user -ne 0 ]; then
   echo "you are not root user"
   exit 1
@@ -31,9 +29,9 @@ fi  }
 
 for package in $@
 do 
-dnf list installed $package &>> $logfile
+dnf list installed $package &>>$logfile
 if [ $? -eq 0 ]; then
-  dnf install $package &>> $logfile
+  dnf install $package &>>$logfile
   validate $? $package
 else
   echo -e "$package already installed $y skipping... $n"  
