e51efb99da47973e872a1c081f5bd5926282eda7
krishna-vamsi-8 <nanivamsi008@gmail.com>
2025-10-06 15:12:02 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -3,7 +3,6 @@
 y="\e[33m"
 r="\e[31m"
 n="\e[0m"
-2=$@
 
 user=$(id -u)
 if [ $user -ne 0 ]; then
@@ -19,13 +18,15 @@ else
   echo -e "$2 is  $y successfully installed $n"
 fi  }
 
-echo "dnf install $2 -y"
-validate $? $2
+dnf install $@ -y
+validate $? $@
 
-
-dnf list installed $2 
+checking(){ 
+            dnf list installed $@
 if [ $1 -ne 0 ]; then
  echo "Already  $G $2 Installed $n"
 else
  echo " $r will install $2 $n"
-fi
+fi }
+
+checking $? $@
